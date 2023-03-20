72c8e4434fc4dd4bd46758849b719fe400445288
Mohammed <m97.chahboun@gmail.com>
2023-03-20 18:43:07 +0000

    Configure service items

--- html_template/views.py
+++ html_template/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from html_template.models import Service
+
 # Create your views here.
 
 def template(request):
-    return render(request, 'index.html')
+    services = Service.objects.all()
+    return render(request, 'index.html',context={'services':services})
